17df06d3d3e66b89668751e5f86bfca0c829d2a8
jozsa <allisonjoyweiner@gmail.com>
2019-02-04 20:00:28 +0000

    Betty compliance

--- factors.c
+++ factors.c
@@ -1,13 +1,21 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+/**
+ * main - Factorize numbers
+ * @argc: The count of arguments passed to the program
+ * @argv: Double pointer to arguments passed to the program
+ *
+ * Return: Always 0
+ */
 int main(int argc, char **argv)
 {
 	FILE *fd;
 	char *number = NULL;
 	size_t len;
-	long unsigned int i;
-	long unsigned int num;
+	unsigned long int i;
+	unsigned long int num;
+
 	if (argc != 2)
 	{
 		printf("Usage: factors <file>\n");
@@ -16,7 +24,7 @@ int main(int argc, char **argv)
 	fd = fopen(argv[1], "r");
 	while (getline(&number, &len, fd) != EOF)
 	{
-		num = atoi(number); 
+		num = atoi(number);
 		if (num == 1)
 			printf("1=1*1");
 		for (i = 2; i <= num; i++)
@@ -25,12 +33,12 @@ int main(int argc, char **argv)
 			{
 				if (i == num)
 					i = 1;
-				printf("%lu=%lu*%lu\n", num, num/i, i);
+				printf("%lu=%lu*%lu\n", num, num / i, i);
 				break;
 			}
 		}
 	}
 	free(number);
 	fclose(fd);
-	return 0;
+	return (0);
 }
